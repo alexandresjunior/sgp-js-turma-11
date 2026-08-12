48802b0d7ee026d20d64d30528d6014b0903ea8f
Alexandre Junior <souzajr.alexandre@gmail.com>
2026-08-12 17:48:39 -0300

    Roteamento das páginas com React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login';
+import Pagina404 from './paginas/Pagina404';
 import './App.css';
 
 function App() {
   return (
-    <h1>Olá, mundo!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Login />} />
+        <Route path="/dashboard" element={<></>} />
+        <Route path="/usuarios" element={<></>} />
+        <Route path="/projetos" element={<></>} />
+        <Route path="/tarefas" element={<></>} />
+        <Route path="*" element={<Pagina404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
