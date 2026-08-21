fb34a9fd027df63606e998f7098374164029c8bd
Alexandre Junior <souzajr.alexandre@gmail.com>
2026-08-20 21:39:47 -0300

    Funcoes de atualizar e buscar usuario pelo ID

--- src/servicos/usuarios.js
+++ src/servicos/usuarios.js
@@ -19,6 +19,42 @@ export async function cadastrarUsuario(dadosUsuario, navigate) {
         }
     }).catch((erro) => {
         alert("Erro ao cadastrar usuário.");
-        console.error("Erro ao cadastrar usuário: ", erro)
+        console.error("Erro ao cadastrar usuário: ", erro);
+    });
+}
+
+export async function atualizarUsuario(idUsuario, dadosUsuario, navigate) {
+    await api.put(`/usuarios/${idUsuario}`, dadosUsuario).then((resposta) => {
+        if (resposta.status === 200) {
+            alert("Usuário atualizado com sucesso!");
+            navigate("/usuarios");
+        }
+    }).catch((erro) => {
+        alert("Erro ao atualizar usuário.");
+        console.error("Erro ao atualizar usuário: ", erro);
+    });
+}
+
+export async function buscarUsuarioPeloId(
+    idUsuario,
+    setNome,
+    setCpf,
+    setEmail,
+    setSenha,
+    setDataNascimento,
+    setStatus
+) {
+    await api.get(`/usuarios/${idUsuario}`).then((resposta) => {
+        if (resposta.status === 200) {
+            setNome(resposta.data?.nome);
+            setCpf(resposta.data?.cpf);
+            setEmail(resposta.data?.email);
+            setSenha(resposta.data?.senha);
+            setDataNascimento(resposta.data?.dataNascimento);
+            setStatus(resposta.data?.status);
+        }
+    }).catch((erro) => {
+        alert("Erro ao buscar usuário.");
+        console.error("Erro ao buscar usuário: ", erro);
     });
 }
